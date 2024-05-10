15db73104edf586bab5d1a4579c4662cec8b5162
Filipe Laíns <lains@riseup.net>
2024-05-10 22:27:46 +0100

    python: refactor fetch-data.py
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- python/fetch-data.py
+++ python/fetch-data.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import os
 import pathlib
 import shutil
@@ -16,49 +17,49 @@ sys.path.insert(0, os.fspath(pathlib.Path(__file__).parent.parent))
 import containers.env
 
 
-def download_artifact(artifact: github.Artifact.Artifact, path: pathlib.Path) -> None:
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {os.environ.get("GITHUB_TOKEN")}',
-        'X-GitHub-Api-Version': '2022-11-28',
-    }
-    with requests.get(artifact.archive_download_url, headers=headers, stream=True) as r:
-        r.raise_for_status()
-        with path.open('wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
+class GithubClient:
+    def __init__(self, repo: str, token: str) -> None:
+        self._token = token
+        self._client = github.Github(auth=github.Auth.Token(token))
+        self._repo = self._client.get_repo('FFY00/python-environments')
 
+    def find_artifact(
+        self,
+        run_name: str,
+        artifact_name: str,
+        commit: str,
+        timeout: int | None = None,
+    ) -> github.Artifact.Artifact | None:
+        for run in self._repo.get_workflow_runs(head_sha=commit):
+            if run.name == run_name:
+                break
 
-def find_artifact(name: str, commit: str) -> github.Artifact.Artifact | None:
-    github_client = github.Github(auth=github.Auth.Token(os.environ.get('GITHUB_TOKEN')))
-    repo = github_client.get_repo('FFY00/python-environments')
+        if timeout is None:
+            tries = range(1)
+        elif timeout == 0:
+            tries = itertools.count()  # infinite
+        else:
+            tries = range(timeout)
 
-    for artifact in repo.get_artifacts(name):
-        if artifact.workflow_run.head_sha == commit:
-            return artifact
-    return None
+        for i in tries:
+            if i != 0:
+                time.sleep(1)
+            for artifact in run.get_artifacts():
+                if artifact.name == artifact_name:
+                    return artifact
+        return None
 
-
-def find_artifact(
-    run_name: str,
-    artifact_name: str,
-    commit: str,
-    timeout: int | None = None,
-) -> github.Artifact.Artifact | None:
-    github_client = github.Github(auth=github.Auth.Token(os.environ.get('GITHUB_TOKEN')))
-    repo = github_client.get_repo('FFY00/python-environments')
-
-    for run in repo.get_workflow_runs(head_sha=commit):
-        if run.name == run_name:
-            break
-
-    for i in range(timeout or 1):
-        if i != 0:
-            time.sleep(1)
-        for artifact in run.get_artifacts():
-            if artifact.name == artifact_name:
-                return artifact
-    return None
+    def download_artifact(self, artifact: github.Artifact.Artifact, path: pathlib.Path) -> None:
+        headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {self._token}',
+            'X-GitHub-Api-Version': '2022-11-28',
+        }
+        with requests.get(artifact.archive_download_url, headers=headers, stream=True) as r:
+            r.raise_for_status()
+            with path.open('wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
 
 
 def main() -> None:
@@ -73,17 +74,28 @@ def main() -> None:
         print('Must specify a commit.')
         sys.exit(1)
 
+    github_client = GithubClient(
+        repo='FFY00/python-environments',
+        token=os.environ.get('GITHUB_TOKEN'),
+    )
+
     workdir = args.outdir / '.fetch-data'
     shutil.rmtree(workdir, ignore_errors=True)
     workdir_data = workdir / 'data'
     workdir_data.mkdir(exist_ok=True, parents=True)
 
-    artifact = find_artifact('build', 'image-data', commit, args.timeout)
+    if args.timeout:
+        print(f'Waiting for for artifact (timout={args.timeout}s)...')
+    else:
+        print('Looking for artifact...')
+    artifact = github_client.find_artifact('build', 'image-data', commit, args.timeout)
+
     if not artifact:
         print(f'Could not find artifact for commit {commit}.')
         sys.exit(1)
+    print(f'Found artifact: {artifact}')
 
-    download_artifact(artifact, workdir / 'image-data.zip')
+    github_client.download_artifact(artifact, workdir / 'image-data.zip')
 
     with zipfile.ZipFile(workdir / 'image-data.zip') as z:
         z.extractall(workdir_data)
